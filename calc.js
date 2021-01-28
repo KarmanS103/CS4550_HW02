f5623a76233aef6c15c5729c30513b18e12ab0bf
Karman Singh <karmansingh@Karmans-MacBook-Pro-2.local>
2021-01-27 23:52:41 -0500

    Animal Done, Started on Calc

---
+++ calc.js
@@ -0,0 +1,24 @@
+// References code from Lecture 2
+
+(function () {
+    "use strict";
+
+    function onButtonPress(){
+        let screen = document.getElementById("screen");
+
+        console.log(this.textContent)
+        screen.value = "" + screen.value + this.textContent
+    }
+
+    function init(){
+        let btns = document.getElementsByClassName("number");
+        let add_btns = Array.from(btns);
+
+        add_btns.forEach(function(btn){
+            btn.addEventListener("click", onButtonPress);
+        });
+
+    }
+
+    window.addEventListener("load", init, false)
+})();
